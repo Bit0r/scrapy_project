6c874c251a9be15666dca8d597aff3146b7ba017
Bit0r <nie_wang@outlook.com>
2021-07-30 18:26:30 +0800

    📦 使用加密算法

--- scrapy_project/spiders/xggl.py
+++ scrapy_project/spiders/xggl.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 import scrapy
 from env import xggl
 
@@ -6,18 +8,28 @@ class XgglSpider(scrapy.Spider):
     name = 'xggl'
     allowed_domains = ['hnie.edu.cn']
 
-    custom_settings = {'LOG_LEVEL': 'WARNING', 'LOG_FILE': 'xggl.log'}
+    custom_settings = {
+        'LOG_LEVEL': 'WARNING',
+        'LOG_FILE': 'xggl.log',
+    }
 
     def start_requests(self):
+        passwd_hash = md5(xggl['passwd'].encode()).hexdigest()
+        if len(passwd_hash) > 5:
+            passwd_hash = passwd_hash[:5] + 'a' + passwd_hash[5:]
+        if len(passwd_hash) > 10:
+            passwd_hash = passwd_hash[:10] + 'b' + passwd_hash[10:]
+        passwd_hash = passwd_hash[:-2]
+
         yield scrapy.FormRequest('http://xggl.hnie.edu.cn/website/login',
                                  self.parse_login,
                                  formdata={
                                      'username': xggl['user'],
-                                     'password': xggl['passwd_key']
+                                     'password': passwd_hash
                                  })
 
     def parse_login(self, _):
-        yield scrapy.FormRequest(
+        return scrapy.FormRequest(
             'http://xggl.hnie.edu.cn/content/student/temp/zzdk',
             formdata=xggl['formdata'])
 
